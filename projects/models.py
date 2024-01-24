165aae464b30ea618c5e4b130aa2507f0b4c610e
Kuan-Hung Lin <145557656+kuanhunglingary@users.noreply.github.com>
2024-01-24 11:20:46 +0800

    Update the files in "projects"

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,11 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.ImageField(upload_to="project_images", blank=True)
+    link = models.URLField(
+        help_text="Link to RST. For example 'https://github.com/kuanhunglingary/DjangoPortfolio/blob/main/README.md'",
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.title
